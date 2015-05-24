67ab67e9ccad7834b8127f8be0d8a766543af8fb
Andrea Trentini <andrea.trentini@unimi.it>
2015-05-24 10:22:26 +0200

    ssh script

--- ssh2arduitter.sh
+++ ssh2arduitter.sh
@@ -1,4 +1,5 @@
-ssh root@atrent.it -p 62222
+# senza web tunnel
+#ssh root@atrent.it -p 62222
 
-# e se si vuol anche accedere all'interfaccia web:
-# ssh  root@sl-lab.it -p 62222 -L 8080:localhost:80
+# se si vuol anche accedere all'interfaccia web:
+ssh  root@sl-lab.it -p 62222 -L 8080:localhost:80
