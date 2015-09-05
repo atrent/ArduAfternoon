e5e15a05e06b68963d4fbdadbf45bb8bc9b6190c
Andrea Trentini <andrea.trentini@unimi.it>
2015-09-05 15:08:43 +0200

    aggiunto tweet di feedback

--- Arduitter/engine.sh
+++ Arduitter/engine.sh
@@ -7,29 +7,43 @@ cd $(dirname $0)
 #TODOFILE=todo.tweets
 DONEFILE=done.tweets
 HOST=localhost
+NAMESFILE=names.list
 
-touch $DONEFILE  # ne assicuro l'esistenza, attenzione che deve essere NON vuoto altrimenti grep fallisce!
-
-# TODO: ripulire meglio grep in caso di < > etc.
+rm $NAMESFILE
+touch $DONEFILE $NAMESFILE # ne assicuro l'esistenza, attenzione che deve essere NON vuoto altrimenti grep fallisce!
 
 echo '/search #arduittercommand' | ./ttytter -script | while
  read raw_tweet
 do
+
+ NAME=$(echo $raw_tweet|cut -f2 -d'<'|cut -f1 -d'>')
+ if
+  grep -q $NAME $NAMESFILE
+ then
+  echo "Nome $NAME gia' presente"
+ else
+  echo Nuovo nome: $NAME
+  echo $NAME >> $NAMESFILE
+ fi
+ 
+ 
+ # TODO: ripulire meglio grep in caso di < > etc.
  tweet=$(echo $raw_tweet|tr "<" "@"|tr ">" "@")
+
  #echo ===
  #echo searching $tweet
   
  if
-    grep "$tweet" $DONEFILE  # se e' gia' stato fatto non farlo (pero' urge ogni tanto svuotare DONEFILE)
+    grep "$tweet" "$DONEFILE"  # se e' gia' stato fatto non farlo (pero' urge ogni tanto svuotare DONEFILE)
  then
 	echo NOT doing... $tweet
  else
 	# TODO: check validita' comando
-		echo doing... $tweet
-		
-		echo $(date +%Y%m%d%H%M%S) $tweet >> $DONEFILE
-		
-		COMMAND=$(echo $tweet| sed -n -e 's/^.*#arduittercommand //p'|tr [:upper:] [:lower:] | tr -s " " "/")
+	echo '===>' doing... $tweet
+
+	echo $(date +%Y%m%d%H%M%S) $tweet >> $DONEFILE
+
+	COMMAND=$(echo $tweet| sed -n -e 's/^.*#arduittercommand //p'|tr [:upper:] [:lower:] | tr -s " " "/")
 	if
 		grep -q $(echo $COMMAND|cut -f1 -d/) << HERE
 		forward
@@ -41,9 +55,17 @@ do
 HERE
 	then
 		curl http://$HOST/arduino/$COMMAND/EOC
-		# TODO: feedback su twitter (ho eseguito i comandi di ....)
 	else
-		echo COMANDO NON PERMESSO
+		echo Comando "$COMMAND" NON PERMESSO
 	fi
  fi
 done
+
+	#echo DEBUG eseguiti: $(cat $NAMESFILE)
+
+if
+	test -s $NAMESFILE
+then
+	# TODO: feedback su twitter (ho eseguito i comandi di ....)
+	echo Eseguiti: $(cat $NAMESFILE) |tr -s "\n" " " | ./ttytter
+fi
