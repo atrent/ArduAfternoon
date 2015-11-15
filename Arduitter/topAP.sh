474d7b22c0bdde5fdadbc3d9f5309fc135b1c86e
arduitter <arduitter@atrent.it>
2015-11-15 12:57:41 +0100

    alpha

--- Arduitter/topAP.sh
+++ Arduitter/topAP.sh
@@ -1,26 +1,31 @@
+#!/bin/bash
+
 WIFI="ArduinoAfternoon"
-LIMIT="10"
+LIMIT="25"
 
 muovi(){
+ echo in muovi
  # direzione random (rotate gradi)
  # forward 1
-	if [ $((($RANDOM%2))) -eq 1 ]
+ let RND=$RANDOM%2
+ echo random: $RND
+	if [ $RND -eq 1 ]
 	then
 		curl http://localhost/arduino/rotateleft/1
 	else
 		curl http://localhost/arduino/rotateright/1
 	fi
-	curl http://localhost/arduino/forward/1
+	curl http://localhost/arduino/backward/1
 }
 
 scan()
 {
- sudo iwlist wlan0 scanning 2>/dev/null|egrep 'Address|ESSID|level|Frequency|Encryp'|tr -s ' '|tr -d '\n'|sed 's/Cell/\nCell/g'|sed 's/level=//g'|sort -t ' ' -k 12 -rn|grep -v '^$' | sed '1d' | grep -i $1
+ iwlist wlan0 scanning 2>/dev/null|egrep 'Address|ESSID|level|Frequency|Encryp'|tr -s ' '|tr -d '\n'|sed 's/Cell/\nCell/g'|sed 's/level=//g'|sort -t ' ' -k 12 -rn|grep -v '^$' | sed '1d' | grep -i $1
 }
 
 #echo wlan0:
 previous="100" 
-echo prev: $previous
+#echo prev: $previous
 
 while true
 do
@@ -28,22 +33,29 @@ do
 	then
 		echo girotondo
 		# curl http://localhost/arduino/rotate/4
-		echo $previous
-		echo $LIMIT
+		echo previous: $previous
+		#echo $LIMIT
 		sleep 1
 	else
 		echo not yet
-		echo $previous
-		echo $LIMIT
+		echo previous: $previous
+		#echo $LIMIT
 		curr="0$(scan $WIFI | head -n 1 | cut -d " " -f 12 | tr -d '-')"
+		
+		if [ $curr -eq 0 ]
+		then
+		 continue
+		fi
+		
 		if [ $curr -gt $previous ]
 		then
 			# ci stiamo allontanando ergo back
 			echo backward 1
-			curl http://localhost/arduino/backward/1
+			curl http://localhost/arduino/forward/1
 		else
 			# ci stiamo avvicinando
 			echo muovi
+			
 			muovi
 		fi
 		previous=$curr
