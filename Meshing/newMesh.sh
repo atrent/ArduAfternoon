1568e2fba8161cf6a6a696843da6d77c247c98ad
millerkjc <millerkjc@debian>
2016-04-29 15:36:47 +0200

    primi test newMesh

--- Meshing/newMesh.sh
+++ Meshing/newMesh.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+!/bin/bash
 
-#set -o verbose  # solo per debug
+set -o verbose  # solo per debug
 
 # "source" di un file config (parametri di avvio)
 CFG=$(hostname).cfg
 if
  test -f $CFG
 then
- . $CFG
+ . ./$CFG
  echo $CFG config used
 else
  . mesh.cfg
@@ -15,17 +15,10 @@ else
 fi
 
 
-#if [ $# -lt 1 ]
-#then
-#	echo "Inserire l'ultimo ottetto dell'indirizzo IP"
-#	exit 1
-#fi
-
 #### modprobe IF moduli kernel non vengono caricati al boot
 #### Controllare con "sudo batctl -v"
 #### modprobe batman-adv
 
-#### ifconfig sarebbe obsoleto... TODO: aggiornare a ip
 
 # Selezione Wlan (o usiamo CFG?)
 #if
@@ -39,64 +32,50 @@ fi
 
 
 ##################################################################################
-#if
-# ip a | grep -iq '$BAT'
-#then
-#	ip link set $BAT down
-#	ip link set $WLAN down
-#	batctl if del $WLAN
-#fi
+if
+ ip a | grep -iq '$BAT'
+then
+	ip link set $BAT down
+	ip link set $WLAN down
+	batctl if del $WLAN
+fi
+
+sleep $SLEEP
+ip link set $WLAN mtu 1528
+
+#### iwconfig wlan0 mode ad-hoc #####
+iw $WLAN set type ibss
+
+
+####iwconfig $WLAN channel 1
+#### iwconfig $WLAN enc off
+#### iwconfig $WLAN essid $MESH_NAME #####
+#### iw $WLAN connect $MESH_NAME
+#### No encryption
+ iw dev $WLAN set channel 1
+ iw dev $WLAN connect $MESH_NAME
 ##################################################################################
 
-#### iwconfig sarebbe obsoleto... TODO: aggiornare a iw
-
 ##################################################################################
-#sleep $SLEEP
-#ip link set $WLAN mtu 1528
-
-##### iwconfig wlan0 mode ad-hoc #####
-#iw $WLAN set type ibss
-
-#iwconfig $WLAN channel 1
-#iwconfig $WLAN enc off
-
-##### iwconfig $WLAN essid $MESH_NAME #####
-#iw $WLAN connect $MESH_NAME
-##################################################################################
-
-#ip link set up dev $WLAN
-#ip link set mtu 1532 dev $WLAN
-
-#iwconfig $WLAN mode ad-hoc essid $MESH_NAME ap $MAC channel 1
-#iwconfig $WLAN mode ad-hoc essid $MESH_NAME ap  $MAC channel 1
-#iwconfig $WLAN mode ad-hoc essid $MESH_NAME any channel 1
-
-##################################################################################
-#batctl if add $WLAN
-#batctl if
+batctl if add $WLAN
+batctl if
 ##################################################################################
 
 #### TODO provare a modificare le sleep
 
 ##################################################################################
-#ifconfig $WLAN up
-#sleep $SLEEP
-#ifconfig bat0 up
-#sleep $SLEEP
-##################################################################################
+####ifconfig $WLAN up
+ip link set $WLAN up
 
-# Al posto di <IP> aggiungere l'indirizzo
-#ifconfig bat0 $IP_WLAN.$IP netmask 255.255.255.0 up
+sleep $SLEEP
 
-#ip link set up dev $WLAN
-#ip link set up dev $BAT
+####ifconfig bat0 up
+ip link set $WLAN up
 
-#avahi-autoipd --no-drop-root $BAT &
-##avahi-autoipd --debug $BAT &
+sleep $SLEEP
 
-##################################################################################
-#batctl o
-#batctl gw_mode client
-#######dhclient -v $BAT &
-#batctl o
+batctl o
+batctl gw_mode client
+######dhclient -v $BAT &	DA MODIFICARE
+batctl o
 ##################################################################################
